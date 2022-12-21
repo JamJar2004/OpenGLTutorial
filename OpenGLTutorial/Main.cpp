d8325de529036078fbfe395dde63dcc34a2fa753
JamJar2004 <cutajarjames2004@gmail.com>
2022-12-21 18:17:05 +0100

    Episode 2

--- OpenGLTutorial/Main.cpp
+++ OpenGLTutorial/Main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+#define GLEW_STATIC
+
 #include <SDL2/SDL.h>
 #include <glm/glm.hpp>
 #include <GL/glew.h>
@@ -8,7 +10,43 @@ int main(int argc, char** argv)
 {
     if(SDL_Init(SDL_INIT_EVERYTHING) == 0)
     {
+        SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+        SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
         SDL_Window* window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+
+        SDL_GLContext context = SDL_GL_CreateContext(window);
+
+        if(glewInit() != GLEW_OK)
+        {
+            std::cout << "Error: Glew failed to initialize." << std::endl;
+            return -1;
+        }
+
+        glClearColor(0.5f, 0.0f, 1.0f, 1.0f);
+
+        GLuint vaoID;
+        glGenVertexArrays(1, &vaoID);
+        glBindVertexArray(vaoID);
+
+        glm::vec3 vertices[] =
+        {
+            glm::vec3(-1, -1, 0),
+            glm::vec3( 0,  1, 0),
+            glm::vec3( 1, -1, 0)
+        };
+
+        GLuint vboID;
+        glGenBuffers(1, &vboID);
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, 3 * sizeof(glm::vec3), vertices, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)0);
 
         bool isClosed = false;
         while(!isClosed)
@@ -23,7 +61,15 @@ int main(int argc, char** argv)
                         break;
                 }
             }
+
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+            SDL_GL_SwapWindow(window);
         }
+
+        SDL_GL_DeleteContext(context);
 
         SDL_DestroyWindow(window);
 
@@ -31,3 +77,4 @@ int main(int argc, char** argv)
     }
     return 0;
 }
+
